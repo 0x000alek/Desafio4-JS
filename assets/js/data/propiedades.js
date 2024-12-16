b5a957c7b2ed348ecebd223dbf7460073013d9ca
0x000alek <aolguin.or@gmail.com>
2024-12-15 22:11:49 -0300

    propiedades en alquiler

---
+++ assets/js/data/propiedades.js
@@ -0,0 +1,48 @@
+const propiedadesAlquiler = [
+    {
+        nombre: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2000,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        baños: 3,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Condominio moderno en zona residencial',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2200,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Cabaña estilo mediterráneo alejada de la ciudad',
+        src: 'https://plus.unsplash.com/premium_photo-1687960116689-38c34910d26f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvbGlkYXklMjBob21lfGVufDB8fDB8fHww',
+        descripcion: 'Esta hermosa cabaña estilo Mediterraneo ofrece un espacio de mucha tranquilidad rodeado de naturaleza',
+        ubicacion: '678 Calm Boulevard, Seaside Town, FL 34567',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2700,
+        smoke: false,
+        pets: true
+    }
+]
+
+export { propiedadesVenta, propiedadesAlquiler }
