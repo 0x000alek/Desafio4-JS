8b6081765ee7646e4c91edbb67595d3349671c24
0x000alek <aolguin.or@gmail.com>
2024-12-15 22:44:22 -0300

    propiedades en venta

--- assets/js/data/propiedades.js
+++ assets/js/data/propiedades.js
@@ -1,3 +1,50 @@
+const propiedadesVenta = [
+    {
+        nombre: 'Apartamento de lujo en zona exclusiva',
+        src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+        descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+        ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+        habitaciones: 4,
+        baños: 4,
+        costo: 5000,
+        smoke: false,
+        pets: false
+    },
+    {
+        nombre: 'Apartamento acogedor en la montaña',
+        src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+        descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+        ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+        habitaciones: 2,
+        baños: 1,
+        costo: 1200,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: 'Penthouse de lujo con terraza panorámica',
+        src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+        descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+        ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+        habitaciones: 3,
+        baños: 3,
+        costo: 4500,
+        smoke: false,
+        pets: true
+    },
+    {
+        nombre: 'Cabaña acogedora en medio del bosque',
+        src: 'https://images.unsplash.com/photo-1523391139596-e7da8a18c733?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        descripcion: 'Esta cabaña se encuentra ubicada en medio de la naturaleza, cercana a un hermoso lago',
+        ubicacion: '456 Winter Road, Lake Michgamme, MI 87263',
+        habitaciones: 4,
+        baños: 3,
+        costo: 3700,
+        smoke: true,
+        pets: true
+    }
+]
+
 const propiedadesAlquiler = [
     {
         nombre: 'Apartamento en el centro de la ciudad',
---
+++ assets/js/propiedades_venta.js
@@ -0,0 +1,60 @@
+import { propiedadesVenta } from './data/propiedades.js'
+
+const propiedadesVentaContainer = document.querySelector('#propiedades-venta')
+const noFumarMessage = `<p class="text-danger">
+                            <i class="fas fa-smoking-ban"></i> No se permite fumar
+                        </p>`
+const siFumarMessage = `<p class="text-success">
+                            <i class="fas fa-smoking"></i> Permitido fumar
+                        </p>`
+const noMascotasMessage = `<p class="text-danger">
+                            <i class="fa-solid fa-ban"></i> No se permiten mascotas
+                        </p>`
+const siMascotasMessage = `<p class="text-success">
+                            <i class="fas fa-paw"></i> Mascotas permitidas
+                        </p>`
+
+const formatearPrecio = (valor) => {
+    return new Intl.NumberFormat('es-CL', {currency: 'CLP', style: 'currency'}).format(valor)
+}
+
+const cargarPropiedades = (propiedades) => {
+    propiedades.forEach(e => {
+        let cardElement = document.createElement('div')
+        let cardContent = `
+            <div class="card">
+              <img
+                src="` + `${e.src}` + `"
+                class="card-img-top"
+                alt="Imagen del departamento"
+              />
+              <div class="card-body">
+                <h5 class="card-title">` +
+                  `${e.nombre}` +
+                `</h5>
+                <p class="card-text">` + 
+                  `${e.descripcion}` +
+                `</p>
+                <p>
+                  <i class="fas fa-map-marker-alt"></i>` + ` ${e.ubicacion}` +
+                `</p>
+                <p>
+                  <i class="fas fa-bed"></i>` + ` ${e.habitaciones}` + ` Habitaciones |
+                  <i class="fas fa-bath"></i>` + ` ${e.baños}` + ` Baños
+                </p>
+                <p><i class="fas fa-dollar-sign"></i>` + ` ${formatearPrecio(e.costo).slice(1)}` + `</p>` +
+                `${e.smoke ? siFumarMessage : noFumarMessage}` + 
+                `${e.pets ? siMascotasMessage : noMascotasMessage}` + 
+              `</div>
+            </div>`
+
+        cardElement.classList.add('col-md-4')
+        cardElement.classList.add('mb-4')
+        cardElement.innerHTML = cardContent
+
+        propiedadesVentaContainer.appendChild(cardElement)
+    })
+}
+
+cargarPropiedades(propiedadesVenta)
+
